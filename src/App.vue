323b33ff5566e9f243b723e9a4582907922398e9
amir78729 <amir78729@gmail.com>
2020-07-27 13:19:36 +0430

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark my-nav">
+            <h5 class="display-5 text-light">Lyric Finder</h5>
+        </nav>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+    .my-nav:hover {
+        opacity: 0.97;
+    }
+
+</style>
